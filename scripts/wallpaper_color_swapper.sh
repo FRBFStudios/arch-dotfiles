cfea042e92258d3ba74960f19188e323b0453880
FRBF Studios <tfroempp@outlook.de>
2026-04-14 12:54:16 +0200

    Fixed wallpapers

--- scripts/wallpaper_color_swapper.sh
+++ scripts/wallpaper_color_swapper.sh
@@ -2,10 +2,10 @@
 
 sleep 0.1
 
-WALLPAPER=$(swww query | sed -n 's/.*image: \(.*\)$/\1/p')
+WALLPAPER=$(awww query | sed -n 's/.*image: \(.*\)$/\1/p')
 
 wal -i $WALLPAPER -e -n -s -t
 
-matugen image $WALLPAPER
+matugen image $WALLPAPER --source-color-index 0
 ln -sf ~/.cache/wal/colors-kitty.conf ~/.config/kitty/16-colors.conf
 ~/.config/matugen/papirus-folders/change-icons.sh
